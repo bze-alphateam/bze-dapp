e148e94fce59ca492fce68ac901c8fb7b704af6e
stefan.balea <stefan.balea@axsys.ro>
2024-07-20 16:13:37 +0300

    added verified for vdl

--- config/verified.ts
+++ config/verified.ts
@@ -6,6 +6,7 @@ export const VERIFIED_TOKENS: { [key: string]: boolean } = {
   "factory/testbz1z3mkcr2jz424w6m49frgjmy9uhlrx69p4cvrgf/bitcoinz": true,
   "ubze": true,
   "utbz": true,
+  "factory/bze13gzq40che93tgfm9kzmkpjamah5nj0j73pyhqk/uvdl": true,
 }
 
 export const EXCLUDED_TOKENS: { [key: string]: boolean } = {
