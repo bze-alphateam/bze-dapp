4175d6440ea494a13c290fac273624e12f07aa12
stefan.balea <stefan.balea@axsys.ro>
2024-12-12 22:50:46 +0200

    added jkl and flix rpc endpoints

--- services/Client.tsx
+++ services/Client.tsx
@@ -21,6 +21,14 @@ export function getNobleRpcUrl(): string {
     return process.env.NEXT_PUBLIC_RPC_URL_NOBLE !== undefined ? process.env.NEXT_PUBLIC_RPC_URL_NOBLE : '';
 }
 
+export function getJackalRpcUrl(): string {
+    return process.env.NEXT_PUBLIC_RPC_URL_JACKAL !== undefined ? process.env.NEXT_PUBLIC_RPC_URL_JACKAL : '';
+}
+
+export function getOmniFlixRpcUrl(): string {
+    return process.env.NEXT_PUBLIC_RPC_URL_FLIX !== undefined ? process.env.NEXT_PUBLIC_RPC_URL_FLIX : '';
+}
+
 export async function getRestClient() {
     return bze.ClientFactory.createLCDClient({restEndpoint: getRestURL()})
 }
@@ -35,6 +43,10 @@ export async function getSigningClient(offlineSigner: any, chainName?: string): 
             return getSigningBzeClient({rpcEndpoint: getOsmosisRpcUrl(), signer: offlineSigner});
         case 'noble':
             return getSigningBzeClient({rpcEndpoint: getNobleRpcUrl(), signer: offlineSigner});
+        case 'jackal':
+            return getSigningBzeClient({rpcEndpoint: getJackalRpcUrl(), signer: offlineSigner});
+        case 'omniflixhub':
+            return getSigningBzeClient({rpcEndpoint: getOmniFlixRpcUrl(), signer: offlineSigner});
         default:
             return getSigningBzeClient({rpcEndpoint: getRpcURL(), signer: offlineSigner});
     }
