8a149e94b9b7cf8a394600b2fbcfc109279244b5
stefan.balea <stefan.balea@axsys.ro>
2025-01-26 22:34:05 +0200

    modified campaign banner

--- pages/trade/market.tsx
+++ pages/trade/market.tsx
@@ -465,10 +465,10 @@ export default function MarketPair() {
                         </Box>
                         <Box flex={1} display={"flex"} p={"$6"} flexDirection={"column"}>
                             <Text fontSize={'$md'} fontWeight={'$bold'}>💰 Rewards Pool: 400,000 $BZE + 2,000 $FLIX</Text>
-                            <Text fontSize={'$md'} fontWeight={'$bold'}>📅 Duration: Jan 20, 2 PM UTC – Jan 25, 2 PM UTC</Text>
+                            <Text fontSize={'$md'} fontWeight={'$bold'}>📅 Campaign ended. Prizes will be sent soon</Text>
                         </Box>
                         <Box display={"flex"} p={"$6"} gap={"$2"} flexDirection={{mobile: 'row', desktop: 'row'}} justifyContent={"flex-end"}>
-                            <Button size={"sm"} intent={"secondary"} rightIcon={"externalLinkLine"} onClick={() => window.open("https://x.com/FlixFanatics/status/1881332778144501943", "_blank", "noopener,noreferrer")}>Read More</Button>
+                            <Button size={"sm"} intent={"secondary"} rightIcon={"externalLinkLine"} onClick={() => window.open("https://docs.google.com/spreadsheets/d/1Tk5ObUcEwvtDFy51i6-2qEQAhEaD0zDZq4H_hG9pK9E/edit?usp=sharing", "_blank", "noopener,noreferrer")}>Winners</Button>
                         </Box>
                     </DefaultBorderedBox>
                     <Box display="flex" flexDirection={{desktop: "row", mdMobile: "row", mobile: "column"}} gap="$4"
